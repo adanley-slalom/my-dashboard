34b1358463bb4e6871cd235a1d1750c3c983f7d2
Aaron Danley <aaron.danley@slalom.com>
2026-08-24 10:46:40 -0400

    Latest Updates

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: false,
+  },
 })
